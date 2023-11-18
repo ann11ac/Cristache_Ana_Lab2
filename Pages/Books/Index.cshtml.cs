207a2ff2c1f9feefa1123e790712d907fa4aceb5
ann11ac <cristacheanacraita@gmail.com>
2023-11-18 11:29:33 +0200

    lab3-updated.

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -30,6 +30,7 @@ namespace Cristache_Ana_Lab2.Pages.Books
 
             BookD.Books = await _context.Book
             .Include(b => b.Publisher)
+            .Include(b => b.Author)
             .Include(b => b.BookCategories)
             .ThenInclude(b => b.Category)
             .AsNoTracking()
